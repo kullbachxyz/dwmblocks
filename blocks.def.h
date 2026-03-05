a7f15a4d9f2dcef715d6f753e9085337bbee42ca
kullbachxyz <philipp@kullbach.net>
2026-03-05 10:10:49 +0100

    move statusbar scripts to ~/.local/bin/

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "$HOME/.local/src/dwmblocks/scripts/wg.sh",	30,		7},
-	{"", "$HOME/.local/src/dwmblocks/scripts/mail.sh",	60,		6},
-	{"", "$HOME/.local/src/dwmblocks/scripts/mpd.sh",	5,		5},
-	{"", "$HOME/.local/src/dwmblocks/scripts/bluetooth.sh",	5,		1},
-	{"", "$HOME/.local/src/dwmblocks/scripts/network.sh",	5,		2},
-	{"", "$HOME/.local/src/dwmblocks/scripts/battery.sh",	30,		3},
-	{"", "$HOME/.local/src/dwmblocks/scripts/clock.sh",	60,		4},
+	{"", "sb-wg",	30,		7},
+	{"", "sb-mail",	60,		6},
+	{"", "sb-mpd",	5,		5},
+	{"", "sb-network",	5,		2},
+	{"", "sb-battery",	30,		3},
+	{"", "sb-clock",	60,		4},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
