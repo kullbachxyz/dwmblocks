a5c5e9ef472f48140b2c723d26a3c657d65c0d51
kullbachxyz <philipp@kullbach.net>
2026-03-13 08:58:24 +0100

    add volume an mic blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-wg",	30,		7},
-	{"", "sb-mail",	60,		6},
-	{"", "sb-mpd",	5,		5},
-	{"", "sb-battery",	30,		3},
-	{"", "sb-clock",	60,		4},
-	{"", "sb-network",	5,		2},
-	{"", "sb-sysact",	60,		1},
+
+	{"", "sb-wg",		30,	7},
+	{"", "sb-mail",		60,	6},
+	{"", "sb-mpd",		5,	5},
+
+	{"", "sb-volume",	1,	8},   // 🔊 speaker volume
+	{"", "sb-mic",		1,	9},   // 🎙️ mic status
+
+	{"", "sb-battery",	30,	3},
+	{"", "sb-clock",	60,	4},
+	{"", "sb-network",	5,	2},
+	{"", "sb-sysact",	60,	1},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
