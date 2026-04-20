d7ed9749774938a6ef0831ba79bd2683dcae5d90
kullbachxyz <ph@kullbach.net>
2026-04-20 19:36:56 +0200

    switch to statusbar scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"","sb-volume",	   0,		10},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "sb-battery",	60,		0},
+
+	{"", "sb-clock",		60,		0},
+
+	{"", "sb-network",	10,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
