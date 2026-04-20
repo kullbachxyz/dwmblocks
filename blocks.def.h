5fe15252a5a27800e855c600177f3fe9dfccaab0
kullbachxyz <ph@kullbach.net>
2026-04-20 21:04:59 +0200

    add statuscmd patch

--- blocks.def.h
+++ blocks.def.h
@@ -3,13 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"","sb-volume",	   0,		10},
 
-	{"", "sb-battery",	60,		0},
+	{"", "sb-battery",	60,		11},
 
-	{"", "sb-clock",		60,		0},
+	{"", "sb-clock",		60,		12},
 
-	{"", "sb-network",	10,		0},
+	{"", "sb-network",	10,		13},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = "  ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 2;
