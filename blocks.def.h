3b5a7852774effc50b9b6a83a9e749a1cae072f8
kullbachxyz <ph@kullbach.net>
2026-05-29 23:14:49 +0200

    update scripts paths

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"","sb-volume",	   0,		10},
+	{"","~/.local/bin/statusbar/sb-volume",	   0,		10},
 
-	{"", "sb-battery",	60,		11},
+	{"", "~/.local/bin/statusbar/sb-battery",	60,		11},
 
-	{"", "sb-clock",		60,		12},
+	{"", "~/.local/bin/statusbar/sb-clock",		60,		12},
 
-	{"", "sb-network",	10,		13},
+	{"", "~/.local/bin/statusbar/sb-network",	10,		13},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
