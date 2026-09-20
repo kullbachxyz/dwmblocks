e0a43b41a61647fc12c29a6a6431140450aa9e90
kullbachxyz <ph@kullbach.net>
2026-09-20 13:47:52 +0200

    add new blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"","~/.local/bin/statusbar/sb-volume",	   0,		10},
-	{"", "~/.local/bin/statusbar/sb-battery",	60,		11},
-	{"", "~/.local/bin/statusbar/sb-clock",		60,		12},
-	{"", "~/.local/bin/statusbar/sb-network",	10,		13},
+	{"","~/.local/bin/sb-cpu",	     1,		14},
+	{"","~/.local/bin/sb-disk",	     2,		13},
+	{"","~/.local/bin/sb-memory",	   1,		12},
+	{"", "~/.local/bin/sb-battery",	60,		11},
+	{"","~/.local/bin/sb-volume",	   0,		10},
+	{"", "~/.local/bin/sb-clock",		 1,		 9},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = "  ";
-static unsigned int delimLen = 2;
+static char delim[] = " | ";
+static unsigned int delimLen = 3;
